2c63924a3c279fef0757e1b907ba00828ab83a89
k473r1nk4 <61155867+k473r1nk4@users.noreply.github.com>
2020-03-19 13:45:45 +0300

    Add files via upload

--- tb.py
+++ tb.py
@@ -2,6 +2,7 @@ import telebot
 import json
 import requests
 import re
+import urllib.parse
 from telebot import types
 
 bot = telebot.TeleBot("1008578392:AAEP2V3AX9GHBWOGtDY4jSep3vkpHeEcoqY")
@@ -28,7 +29,7 @@ def send_text(message):
     elif message.text.lower() == 'кто ты?':
         bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAILn15hVGiGt5sAAc4oAmycLaqCNFm6zwACDGwBAAFji0YMe4_uoQbUVQEYBA')
     elif re.fullmatch(r'(\b\w+\b)', message.text.lower()):
-        search=api+message.text.lower()
+        search=api+urllib.parse.quote_plus(message.text.lower())
         response = requests.get(search)
         bot.send_message(message.chat.id, response.json()['list'][0]['definition']) 
     else:
@@ -37,23 +38,29 @@ def send_text(message):
 
 @bot.inline_handler(func=lambda query: len(query.query) > 0)
 def query_text(query):
-    try:
-        matches = re.match(digits_pattern, query.query)
-    except AttributeError as ex:
-        return
-    text = matches.group()
-    search=api+text.lower()
-    response = requests.get(search)
-    a=response.json()['list'][0]['definition']
-    r_sum = types.InlineQueryResultArticle(
-        id='1', title="Result",
-        description="Запрос: {!s}".format(a),
-        input_message_content=types.InputTextMessageContent(
-        message_text=a)
-    )
-    k=False
-    bot.answer_inline_query(query.id, [r_sum])
-    
+    #try:
+    #    matches = re.match(digits_pattern, query.query)
+    #except AttributeError as ex:
+    #    return
+    if query.query != '':
+    #    matches = query.query
+    #else:
+    #    return
+    #matches = query.query
+        #text = matches.group()
+        text= query.query
+        search=api+urllib.parse.quote_plus(text.lower())
+        response = requests.get(search)
+        a=response.json()['list'][0]['definition']
+        r_sum = types.InlineQueryResultArticle(
+            id='1', title="Result",
+            description="Запрос: {!s}".format(a),
+            input_message_content=types.InputTextMessageContent(
+            message_text=a)
+        )
+        k=False
+        bot.answer_inline_query(query.id, [r_sum])
+        
 
 
 @bot.message_handler(content_types=['sticker'])
